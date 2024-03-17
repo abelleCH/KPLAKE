4aea4266a2adc69ef801da9c938427593ab633df
PC IN Martin <theoburdinat@outlook.fr>
2024-03-17 14:46:44 +0100

    update

--- Kp_lake/extrac_sharepoint.py
+++ Kp_lake/extrac_sharepoint.py
@@ -12,7 +12,7 @@ else:
 # site_url = "https://connectin.ingroupe.com/team/SfFrontOffice/SitePages/Accueil.aspx"
 # file_relative_url = "RootFolder=%2Fteam%2FSfFrontOffice%2FDocuments%20partages%2F02%20-%20Squad%20Identity%20Station&FolderCTID=0x012000171359A7C53D494DB5D00E85CEC98595&View=%7BC4D67FB1-BE2D-45C5-9904-02C48F283546%7D"  # Spécifiez le chemin relatif du fichier à partir du site SharePoint
 
-# # URL de l'API REST pour obtenir les informations sur le fichier spécifié
+# # URL de l'API REST pour obtenir les informations sur le fichier spécifiégi
 # api_url = f"{site_url}/_api/web/getfilebyserverrelativeurl('{file_relative_url}')"
 
 # # Headers requis pour spécifier le type de contenu et l'authentification
