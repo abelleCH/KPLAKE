a5c82992eba6fd4c1fa9038420c51e97a74b12a1
abelle_ch <abelle.charlemagne@epfedu.fr>
2024-03-17 15:39:33 +0100

    a

--- Kp_lake/extrac_sharepoint.py
+++ Kp_lake/extrac_sharepoint.py
@@ -1,40 +1,21 @@
 import requests
 
-# URL de l'API SharePoint pour obtenir les informations sur le dossier "Documents partages"
-api_url = "https://connectin.ingroupe.com/team/SfFrontOffice/_api/web/getfolderbyserverrelativeurl('/team/SfFrontOffice/Documents partages')"
+# URL de l'API SharePoint pour obtenir les informations sur les fichiers
+api_url = "https://connectin.ingroupe.com/team/SfFrontOffice/_api/web/getfolderbyserverrelativeurl('/team/SfFrontOffice/Documents%20partages/02%20-%20Squad%20Identity%20Station')/files"
 
 # Remplacez 'votre_nom_utilisateur' et 'votre_mot_de_passe' par vos identifiants SharePoint
 username = 'tburdinat'
 password = '?MWRL1R8if'
 
-# Envoyer une requête GET à l'API SharePoint pour obtenir les informations sur le dossier "Documents partages"
+# Envoyer une requête GET à l'API SharePoint pour obtenir les informations sur les fichiers
 response = requests.get(api_url, auth=(username, password))
 
 # Vérifier si la requête a réussi
 if response.status_code == 200:
     data = response.json()
-
-    # Parcourir les dossiers pour trouver le dossier "02 - Squad Identity Station"
-    for folder in data['Folders']:
-        if folder['Name'] == '02 - Squad Identity Station':
-            # Récupérer l'URL du dossier "02 - Squad Identity Station"
-            folder_url = "%2Fteam%2FSfFrontOffice%2FDocuments%20partages%2F02%20-%20Squad%20Identity%20Station&FolderCTID=0x012000171359A7C53D494DB5D00E85CEC98595&View=%7BC4D67FB1-BE2D-45C5-9904-02C48F283546%7D"
-
-            # Récupérer les fichiers du dossier "02 - Squad Identity Station"
-            files_url = f"https://connectin.ingroupe.com/team/SfFrontOffice/_api/web/getfolderbyserverrelativeurl('{folder_url}')/files"
-            files_response = requests.get(files_url, auth=(username, password))
-
-            # Vérifier si la requête a réussi
-            if files_response.status_code == 200:
-                files_data = files_response.json()
-
-                # Parcourir les fichiers pour trouver le fichier "02 - Squad Identity Station"
-                for file in files_data['value']:
-                    if file['Name'] == '02 - Squad Identity Station':
-                        print("Nom du fichier:", file['Name'])
-                        print("URL du fichier:", file['ServerRelativeUrl'])
-                        print("Taille du fichier:", file['Length'])
-            else:
-                print("Erreur lors de la récupération des fichiers du dossier:", files_response.status_code)
+    # Parcourir les fichiers et afficher leurs URLs
+    for file in data['value']:
+        print("Nom du fichier:", file['Name'])
+        print("URL du fichier:", file['ServerRelativeUrl'])
 else:
-    print("Erreur lors de la récupération des informations sur le dossier:", response.status_code)
+    print("Erreur lors de la récupération des fichiers:", response.status_code)
